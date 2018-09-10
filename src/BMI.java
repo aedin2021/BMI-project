77656e7a8552d6e42ce97754e648b97e9d379dec
aedin <aedin.rundell.2021@student.lhschools.org>
2018-09-10 14:18:22 -0400

    BMI2

--- src/BMI.java
+++ src/BMI.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
+
 public class BMI {
     public static void main (String[] args){
 
-
+        Scanner key;
+        key = new Scanner(System.in);
+        int kg;
+        int m;
+        String BMI;
+        BMI = "BMI=Kg*M^2";
+        System.out.println(BMI);
 
 
 
